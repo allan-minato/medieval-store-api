49aee0db93ab6232725ff24ebba4060acff32d0b
Allan Medeiros Minato <allan.minato@gmail.com>
2023-05-30 13:06:26 -0300

    Resolucao requisito 05

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,12 +3,10 @@ import productService from '../services/product.service';
 
 async function createProduct(req: Request, res: Response): Promise<Response> {
   const { body } = req;
-  // if (!body.name || !body.price || !body.orderId) {
-  //   return res.status(400).json({ message: 'One or more properties are missing.' });
-  // }
+
   const create = await productService.createProduct(body);
 
-  return res.status(201).json(create);
+  return res.status(201).json(create.dataValues);
 }
 
 async function listProduct(_req: Request, res: Response): Promise<Response> {
--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import productController from '../controllers/product.controller';
+import productsMiddleware from '../database/middlewares/validateProducts';
 
 const productsRouter = Router();
 
-productsRouter.post('/products', productController.createProduct);
+productsRouter.post(
+  '/products',
+  productsMiddleware.validateProduct,
+  productsMiddleware.validatePrice, 
+  productController.createProduct,
+);
 productsRouter.get('/products', productController.listProduct);
 
 export default productsRouter;
